0ef97f9959fc116323583f94b65566ba0f9d890b
janneskruse <jannes.kruse@rwth-aachen.de>
2024-06-24 04:12:11 +0200

    restructured slurm download scripts

--- code/slurm/01_download_rhone.sh
+++ code/slurm/01_download_rhone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --time=1-23:00:00  # Tag-Stunden
-#SBATCH --job-name="unzip_rhone"
+#SBATCH --job-name="01_download_rhone"
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4  # Utilize multiple CPUs for parallel processing
@@ -9,10 +9,13 @@
 #SBATCH --partition=paul-long
 #SBATCH --mail-user=cu19icep@studserv.uni-leipzig.de  # Please indicate your own email here
 #SBATCH --mail-type=ALL 
-#SBATCH -o "Unzip_Rhone.%j.txt"  # j for the job id
+#SBATCH -o "01_download_rhone.%j.txt"  # j for the job id
 
-# ACTIVATE ENV
-source /home/sc.uni-leipzig.de/le837wmue/Big-Data-Praktikum/lexcube/bin/activate
+# Determine the root directory of the Git repository
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# Navigate to the root directory
+cd "$REPO_ROOT"
 
 # RUN SCRIPT
-
+poetry run python code/slurm/01_download_rhone.py
