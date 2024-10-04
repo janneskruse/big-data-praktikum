4311baa957ffe7399dec5f243fa434b5a1752fdb
janneskruse <105678810+janneskruse@users.noreply.github.com>
2024-10-04 20:32:33 +0200

    remove poetryy part from pipeline sbatch

--- code/slurm/02_fft_pipeline.sh
+++ code/slurm/02_fft_pipeline.sh
@@ -14,24 +14,9 @@
 source /home/sc.uni-leipzig.de/${USER}/.bashrc
 source activate rhoneCube
 
-
-# Determine the root directory of the Git repository
-REPO_ROOT=$(git rev-parse --show-toplevel)
-
-# Navigate to the root directory
-cd "$REPO_ROOT"
-
 # Calculate the total number of CPUs allocated by SLURM for this job
 TOTAL_CPUS=$((SLURM_NTASKS * SLURM_CPUS_PER_TASK))
 echo "Total CPUs: $TOTAL_CPUS"
 
 # Pass this number to the Python script, which can use it to set the number of processes in multiprocessing.Pool
-#poetry run python -u code/slurm/02_fft_pipeline.py $TOTAL_CPUS
-python3 -u code/slurm/02_fft_pipeline.py $TOTAL_CPUS
-
-# # Print CPU information for debugging
-# echo "SLURM job allocated CPUs: $SLURM_CPUS_ON_NODE"
-# echo "Multiprocessing CPU count: $(python -c 'import multiprocessing as mp; print(mp.cpu_count())')"
-
-# # RUN SCRIPT
-# poetry run python -u code/slurm/02_fft_pipeline.py
+python3 -u 02_fft_pipeline.py $TOTAL_CPUS
